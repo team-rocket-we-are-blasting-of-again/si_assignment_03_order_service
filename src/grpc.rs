f8c6403fe8b048a6364d33258467b2bb29fe189d
Frederik Riisager Johnsen <frederik@familienjohnsen.dk>
2022-11-08 13:59:50 +0100

    formatting

--- src/grpc.rs
+++ src/grpc.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::error::Error;
 
 use crate::students::{
     book_purchase_service_client::BookPurchaseServiceClient, BookToBuy, BoughtBookReply,
@@ -8,7 +9,7 @@ use crate::students::{
 pub async fn student_has_funds(
     price: f64,
     student_id: i32,
-) -> Result<bool, Box<dyn std::error::Error + Send + Sync>> {
+) -> Result<bool, Box<dyn Error + Send + Sync>> {
     let url = env::var("STUDENT_SERVICE_URL").unwrap();
     let mut client = BookPurchaseServiceClient::connect(url).await?;
     let request = tonic::Request::new(StudentInfo {
@@ -24,7 +25,7 @@ pub async fn order(
     book_id: i32,
     student_id: i32,
     price: f64,
-) -> Result<BoughtBookReply, Box<dyn std::error::Error + Send + Sync>> {
+) -> Result<BoughtBookReply, Box<dyn Error + Send + Sync>> {
     let url = env::var("STUDENT_SERVICE_URL").unwrap();
     let mut client = BookPurchaseServiceClient::connect(url).await?;
     let request = tonic::Request::new(BookToBuy {
--- src/order_service/mod.rs
+++ src/order_service/mod.rs
@@ -5,11 +5,12 @@ pub mod order_service {
         students::BoughtBookReply,
         types::Title,
     };
+    use std::error::Error;
 
     pub async fn can_order_book(
         book: &Title,
         student_id: i32,
-    ) -> Result<bool, Box<dyn std::error::Error + Send + Sync>> {
+    ) -> Result<bool, Box<dyn Error + Send + Sync>> {
         let student_response = student_has_funds(book.price, student_id).await;
 
         match student_response {
@@ -21,7 +22,7 @@ pub mod order_service {
     pub async fn make_order(
         book: &Title,
         student_id: i32,
-    ) -> Result<BoughtBookReply, Box<dyn std::error::Error + Send + Sync>> {
+    ) -> Result<BoughtBookReply, Box<dyn Error + Send + Sync>> {
         order(book.id, student_id, book.price).await
     }
 }
